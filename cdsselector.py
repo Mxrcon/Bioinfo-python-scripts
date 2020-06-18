1e3cde218372d822d84588d4f94d69a4e4033149
Mntrgln <48180517+Mntrgln@users.noreply.github.com>
2020-06-18 20:03:27 -0300

    Update cdsselector.py to 1.0
    Fixed retrieving only cds, now the output has genes, cds and source

--- cdsselector.py
+++ cdsselector.py
@@ -8,49 +8,45 @@
 	1 = nome da pasta com os gbk input
 	2 = nome da pasta para os outputs
 	3 = nome da lista com os genes para serem mantidos
-
 	OBS: A formatação dos genes deve ser mantida de forma
 	simples, apenas uma sigla de gene por linha, que não 
 	deve conter aspas ou espaço.	
-
 	Desenvolvido por Davi J. Marcon
 	Email: Davijosuemarcon@gmail.com
 """
-
 from Bio import SeqIO
 import sys
 import os
-
-
 #Determinando os inputs e outputs
 entrada = sys.argv[1]
 saida = sys.argv[2]
 Lista = [line.rstrip('\n') for line in open(sys.argv[3])]
-
 #abrindo os inputs
 for i in os.listdir(entrada):
 	open(entrada+"/"+i)
-
 #conta o numero de gbs
 list = os.listdir(entrada)
 number_files = len(list)
 print("Foram encontrados" , (number_files), "arquivos")
-
 #cria  a pasta de outputs
 if not os.path.exists(saida):
     os.makedirs(saida)
-
-
 #Executando código
 for i in os.listdir(entrada): 
-	X = []
+	FEATURES = []
 	for seq_record in SeqIO.parse(entrada+"/"+i , 'genbank'):
 		for feature in seq_record.features:
 			if feature.type == "CDS" and "gene" in feature.qualifiers:
 				gene = feature.qualifiers['gene'][0]
 				if gene in Lista:
-					X.append(feature)
-					seq_record.features = X
+					FEATURES.append(feature)
+			if feature.type == "gene" and "gene" in feature.qualifiers:
+				gene = feature.qualifiers['gene'][0]
+				if gene in Lista:
+					FEATURES.append(feature)
+			if feature.type == "source" and "mol_type" in feature.qualifiers:
+				if feature.qualifiers["mol_type"][0] == 'genomic DNA':
+					FEATURES.append(feature)
+			seq_record.features = FEATURES
 	print("Gerando output para: "+i)
 	SeqIO.write (seq_record, saida+"/"+i, 'genbank')
-
