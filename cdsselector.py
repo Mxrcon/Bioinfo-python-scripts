ada9f497b36f4a1ffd452aa6d601b1e033d98705
Mxrcon <davijosuemarcon@gmail.com>
2021-04-19 23:52:27 -0300

    refactoring cds selector

--- cdsselector.py
+++ cdsselector.py
@@ -1,52 +1,58 @@
 # -*- coding:utf-8 -*-
-"""     O objetivo deste script é remover CDS não desejadas, 
-	ao determinar quais as CDS que você deseja manter no
-	GBK. Para isso devem ser utilizados arquivos GBK full
-	devidamente anotados.
-	
-	python a.py 1 2 3
-	1 = nome da pasta com os gbk input
-	2 = nome da pasta para os outputs
-	3 = nome da lista com os genes para serem mantidos
-	OBS: A formatação dos genes deve ser mantida de forma
-	simples, apenas uma sigla de gene por linha, que não 
-	deve conter aspas ou espaço.	
-	Desenvolvido por Davi J. Marcon
-	Email: Davijosuemarcon@gmail.com
+"""    
+	Propouse:
+		This script removes CDS that arent on the allowed list, 
+		with annoted GBK files you can create a GBK that only
+		contains CDS that are ´allowed´ by the list.
+
+	Usage:	
+	python cdsselector.py $GBK_FOLDER $OUTDIR #LIST_FILE
+
+	$GBK_FOLDER = Folder containing all gbk files that will be used as input
+
+	$OUTDIR = Name of the output dir
+
+	$LIST_FILE = A file containt a gene on every line following this example:
+	dnaA
+	dnaK
+	rpoB
+
+	OBS: the list file shouldn't contain and special character.
 """
 from Bio import SeqIO
 import sys
 import os
-#Determinando os inputs e outputs
-entrada = sys.argv[1]
-saida = sys.argv[2]
-Lista = [line.rstrip('\n') for line in open(sys.argv[3])]
-#abrindo os inputs
-for i in os.listdir(entrada):
-	open(entrada+"/"+i)
-#conta o numero de gbs
-list = os.listdir(entrada)
-number_files = len(list)
-print("Foram encontrados" , (number_files), "arquivos")
-#cria  a pasta de outputs
-if not os.path.exists(saida):
-    os.makedirs(saida)
-#Executando código
-for i in os.listdir(entrada): 
+
+#I/O declaration
+
+input_folder = sys.argv[1]
+output_folder = sys.argv[2]
+gene_list = [line.strip('\n') for line in open(sys.argv[3])]
+
+#Taking permission for parsing later
+for i in os.listdir(input_folder):
+	open(input_folder+"/"+i)
+
+#Check output folder existance
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+#Code
+for input_file in os.listdir(input_folder): 
 	FEATURES = []
-	for seq_record in SeqIO.parse(entrada+"/"+i , 'genbank'):
-		for feature in seq_record.features:
+	for seq_record in SeqIO.parse(input_folder+"/"+input_file , 'genbank'):# Parse all files and interact around them
+		for feature in seq_record.features: # Check each feature and see if it is on gene_list
 			if feature.type == "CDS" and "gene" in feature.qualifiers:
 				gene = feature.qualifiers['gene'][0]
-				if gene in Lista:
+				if gene in gene_list:
 					FEATURES.append(feature)
 			if feature.type == "gene" and "gene" in feature.qualifiers:
 				gene = feature.qualifiers['gene'][0]
-				if gene in Lista:
+				if gene in gene_list:
 					FEATURES.append(feature)
 			if feature.type == "source" and "mol_type" in feature.qualifiers:
 				if feature.qualifiers["mol_type"][0] == 'genomic DNA':
-					FEATURES.append(feature)
+					FEATURES.append(feature)# If the feature is on the gene_list append them to the new feature list
 			seq_record.features = FEATURES
-	print("Gerando output para: "+i)
-	SeqIO.write (seq_record, saida+"/"+i, 'genbank')
+	print("Creating input for: "+input_file)
+	SeqIO.write (seq_record, output_folder+"/"+input_file, 'genbank')
